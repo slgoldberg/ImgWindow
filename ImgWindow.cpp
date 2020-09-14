726b53dfb1958533421fca8d154c36152f93891e
TwinFan <twinfan@sonux.net>
2020-09-14 22:34:11 +0200

    Backspace in VR Workaround

--- ImgWindow.cpp
+++ ImgWindow.cpp
@@ -413,6 +413,13 @@ ImgWindow::DrawWindowCB(XPLMWindowID /* inWindowID */, void *inRefcon)
         io.KeysDown[XPLM_VK_BACK] = false;
         thisWindow->bResetBackspace = false;
     }
+    
+    // Hack: Reset the Backspace key if in VR (see HandleKeyFuncCB for details)
+    if (thisWindow->bResetBackspace) {
+        ImGuiIO& io = ImGui::GetIO();
+        io.KeysDown[XPLM_VK_BACK] = false;
+        thisWindow->bResetBackspace = false;
+    }
 }
 
 int
